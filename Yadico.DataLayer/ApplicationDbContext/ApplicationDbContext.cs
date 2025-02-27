09862721f46cb4acd82f2294131b1e7baedf3c26
mrrajabali <mrrajabali@gmail.com>
2025-02-27 23:58:05 +0330

    setup : add dbcontext and configure code first models

---
+++ Yadico.DataLayer/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yadico.DataLayer.Models;
+
+namespace Yadico.DataLayer.ApplicationDbContext
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name = DefaultConnection")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<MotivationMessage> MotivationMessages { get; set; }
+    }
+}
